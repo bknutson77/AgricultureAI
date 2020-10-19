5a7780c43c8ad57e421969c9e326178992ea9700
Ben Knutson <benjk117@gmail.com>
2020-10-18 20:35:01 -0500

    Creating front-end views for logging in, registering, and stubbing out module home page.  Navigation redirection and styling css also included.

--- AgricultureAI/Website/js/site.js
+++ AgricultureAI/Website/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+/* Login/Register/Continue-As-Guest Functions */
+function loginRedirect() {
+    document.location = "Login";
+}
+
+function registerRedirect() {
+    document.location = "Register";
+}
+
+function continueAsGuestRedirect() {
+    document.location = "ModuleHome";
+}
+
+function returnToLoginRegisterHome() {
+    document.location = "Index";
+}
